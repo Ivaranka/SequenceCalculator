88016c62f59b5ed68a5f4d25485859b8f1e4bb21
Ivar Andreas Kaland <ivaranka@gmail.com>
2018-04-18 04:59:48 +0200

    Added final SequenceCalculator and tested it in program.cs

--- SequenceCalculator/Program.cs
+++ SequenceCalculator/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace SequenceCalculator
 {
     class Program
     {
+
+            //Dette programmet løser de n første fibonaccitallene.
+            //To-Do, generaliser talltype vekk fra BigInteger og til alle numre T.
+            //To-Do, Skriv og les inn verdier fra en tekstfil, og fortsett fra disse verdiene.
+            //To-Do, utvid klassen slik at man kan fortsette fra sist.
         static void Main(string[] args)
-        {
+            {
+
+            //Lager en ny instanse av en følge. Beregner de n fibonaccitallene.
+            var Fib = new FibonacciNext();
+            var seriesCalc = new SequenceCalculator(Fib);
+            //BigInteger[] Test=seriesCalc.FillArray(150, new BigInteger[] { 0, 1 });
+            seriesCalc.PopulateList(140, new BigInteger[] { 0, 1 });
+            seriesCalc.ExpandList(100);
+            seriesCalc.ExpandList(200);
+            seriesCalc.Print();
+
+                //Et eksempel på bruk der hvor man bruker en annen rekkeberegning på en ny instanse av følgeberegneren.
+            var Ari = new ArithmetricNext(9);
+            var seriesCalc2 = new SequenceCalculator(Ari);
+            seriesCalc2.PopulateList(15, new BigInteger[] { 1 });
+            seriesCalc2.ExpandList(10);
+            seriesCalc2.Print();
+
+            Console.ReadLine();
         }
+        
     }
 }
---
+++ SequenceCalculator/SequenceCalculator.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.IO;
+using System.Runtime.Serialization;
+namespace SequenceCalculator
+{
+
+        /// <summary>
+        /// En klasse some beregner de n første elementene i en følge
+        /// </summary>
+        [Serializable()]
+        public class SequenceCalculator
+        {
+            public SequenceCalculator(ICalculateNextElement nextFinder)
+            {
+                SavedInts = new List<BigInteger>();
+                NextFinder = nextFinder;
+                _numbersInInitialElements = 0;
+                _populated = false;
+            }
+
+            //Egenskaper og Felter
+            public ICalculateNextElement NextFinder { get; }
+            public List<BigInteger> SavedInts { get; set; }
+            private int _numbersInInitialElements;
+            private bool _populated;
+
+            //Metoder
+            private BigInteger[] FillArray(int numOfElements, BigInteger[] initialElements)
+            {
+
+                BigInteger[] ArrayToFill = new BigInteger[numOfElements];
+                for (int i = 0; i < initialElements.Length; i++)
+                {
+                    ArrayToFill[i] = initialElements[i];
+                }
+                for (int i = initialElements.Length; i < numOfElements; i++)
+                {
+                    ArrayToFill[i] = NextFinder.CalculateNext(ref initialElements);
+                }
+                return ArrayToFill;
+            }
+
+            public void PopulateList(int numOfElements, BigInteger[] initialElements)
+            {
+                _numbersInInitialElements = initialElements.Length;
+                SavedInts.Clear();
+                BigInteger[] newListOfInts = FillArray(numOfElements, initialElements);
+                SavedInts.AddRange(newListOfInts);
+                _populated = true;
+            }
+            public void ExpandList(int numToExpand)
+            {
+                if (_populated)
+                {
+                    var initialElementsExpand = SavedInts.GetRange(SavedInts.Count - _numbersInInitialElements, _numbersInInitialElements).ToArray();
+                    BigInteger[] intsToAdd = FillArray(numToExpand + _numbersInInitialElements, initialElementsExpand);
+                    SavedInts.InsertRange(SavedInts.Count - _numbersInInitialElements, intsToAdd);
+                    SavedInts.RemoveRange(SavedInts.Count - _numbersInInitialElements, _numbersInInitialElements);
+                }
+                else { Console.WriteLine("Please run PopulateList prior to Expanding"); }
+            }
+            public void Print()
+            {
+                for (int i = 0; i < SavedInts.Count; i++)
+                {
+                    Console.WriteLine($"Element {i + 1} of the given sequence is :{SavedInts[i]}");
+                }
+                Console.WriteLine($"Current list length: {SavedInts.Count}");
+            }
+            //To-Do: Serialization
+            //To-Do: Fortsette fra et bestemt tall 
+            //To-Do: Trenger enkapsulering
+        }
+
+        /// <summary>
+        /// Implementerer ICalculateNextElement for fibonaccifølgen.
+        /// </summary>
+        public class FibonacciNext : ICalculateNextElement
+        {
+
+            public BigInteger CalculateNext(ref BigInteger[] a)
+            {
+                BigInteger returnVal = a[0] + a[1];
+                a[0] = a[1];
+                a[1] = returnVal;
+                return returnVal;
+            }
+        }
+
+        /// <summary>
+        /// Implementerer ICalculateNextElement for aritmetiske følger.
+        /// </summary>
+        public class ArithmetricNext : ICalculateNextElement
+        {
+            public ArithmetricNext(int d = 2)
+            {
+                _d = d;
+            }
+            private int _d;
+
+            public BigInteger CalculateNext(ref BigInteger[] a)
+            {
+                BigInteger returnVal = a[0] + _d;
+                a[0] = returnVal;
+                return returnVal;
+            }
+        }
+
+        /// <summary>
+        /// Interface som skal brukes til å utvide funksjonaliteten til SequenceCalculator.
+        /// Implementer CalculateNext slik at den returnerer det neste tallet i rekka, og 
+        /// oppdaterer matrisen a[] med verdier som skal brukes i neste beregning.
+        /// </summary>
+        public interface ICalculateNextElement
+        {
+            BigInteger CalculateNext(ref BigInteger[] a);
+        }
+
+
+    
+
+}
